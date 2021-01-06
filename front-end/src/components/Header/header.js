93829a6a335758a939f756c6842a4b0602a4c0e5
BrunaN <brubsnev@gmail.com>
2021-01-06 12:41:13 -0300

    direcionamento das páginas

--- front-end/src/components/Header/header.js
+++ front-end/src/components/Header/header.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Button } from 'antd';
 import { ArrowRightOutlined, PaperClipOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom'
 
 import './header.scss';
 import history from '../../utils/history';
 
 const Header = () => {
+    const {pathname} = useLocation();
+    console.log(pathname);
     return (
         <div className="header-content padding-page">
             <Link className="brand" to="/">
@@ -15,7 +18,7 @@ const Header = () => {
                     <p>valiDC</p>
                 </div>
             </Link>
-            <Button type="link" onClick={() => {history.push('/document/validation')}}>Validar arquivo <ArrowRightOutlined /></Button>
+            { pathname !== '/document/validation' && <Button type="link" onClick={() => {history.push('/document/validation')}}>Validar arquivo <ArrowRightOutlined /></Button> }
         </div>
     )
 }
