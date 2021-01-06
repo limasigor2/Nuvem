7bc1920e246e8f73f35e08b0a16377bde2901d34
BrunaN <brubsnev@gmail.com>
2021-01-06 09:14:55 -0300

    gerencimento de arquivos do usuário, listagem dos arquivos em cards

--- front-end/src/components/User/FileManager/fileManager.js
+++ front-end/src/components/User/FileManager/fileManager.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Button } from 'antd';
+import { PlusOutlined} from '@ant-design/icons';
 import Card from '../../Card/card';
 
 import './fileManager.scss';
@@ -6,6 +8,10 @@ import './fileManager.scss';
 const FileManager = () => {
     return (
         <div className='file-manager-container padding-page'>
+            <div className="page-header">
+                <h1>Gerenciador de Arquivos</h1>
+                <Button shape="circle" icon={<PlusOutlined />} />
+            </div>
             <div className="card-list">
                 <Card code={'Código do arquivo'} />
             </div>
