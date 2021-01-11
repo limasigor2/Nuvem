cf0ecbf084b2ea030f0092d252c7b90a2d060be8
Igor Lima <limasigor2@gmail.com>
2021-01-11 08:00:01 -0300

    começando upload de arquivos

--- back-end/src/main/java/br/ufc/dc/validc/ValiDcApplication.java
+++ back-end/src/main/java/br/ufc/dc/validc/ValiDcApplication.java
@@ -3,10 +3,16 @@ package br.ufc.dc.validc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import br.ufc.validc.property.FileStorageProperties;
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableConfigurationProperties({
+    FileStorageProperties.class
+})
 public class ValiDcApplication {
 
 	public static void main(String[] args) {
---
+++ back-end/src/main/java/br/ufc/dc/validc/controller/FileController.java
@@ -0,0 +1,14 @@
+package br.ufc.dc.validc.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FileController {
+
+	
+	 private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+	 
+	 
+}
